3ffc0f128f77c2491096980ab040a99f33698824
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-31 20:08:35 +0200

    working on Sidenav component

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import {AiOutlineMenu} from 'react-icons/ai'
 
 export default function Sidenav() {
-  return <div>Sidenav</div>;
+  return <div>
+    <AiOutlineMenu />
+  </div>;
 }

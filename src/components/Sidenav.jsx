8013a03feaf8af9d8edc8e4d47076413943c90a3
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-31 20:43:56 +0200

    customizing the Sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineMenu, AiOutlineHome } from 'react-icons/ai';
 
 export default function Sidenav() {
   const [nav, setNav] = useState(false);
@@ -14,6 +14,19 @@ export default function Sidenav() {
         onClick={handleNav}
         className="z-[99] md:hidden absolute top-4 right-4 cursor-pointer"
       />
+      {nav ? (
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center bg-gray-200 shadow-md rounded-full shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-105 ease-in duration-200"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }

4e017bb7278884813f86bec46603d2ded2248ddf
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-01-31 20:18:52 +0200

    adding mobile menu icon

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import {AiOutlineMenu} from 'react-icons/ai'
+import React, { useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
 
 export default function Sidenav() {
-  return <div>
-    <AiOutlineMenu />
-  </div>;
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    console.log('state changes:', nav);
+  };
+
+  return (
+    <div>
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="z-[99] md:hidden absolute top-4 right-4 cursor-pointer"
+      />
+    </div>
+  );
 }

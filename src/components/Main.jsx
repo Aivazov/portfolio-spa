56b6afb5ccb9ea8890b7c8c02ec5260feb607a86
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-02-02 12:47:11 +0200

    styling Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,8 +17,8 @@ export default function Main() {
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-black/40">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
-          <h1 className="sm:text-5xl text-4xl font-bold text-gray-200">
-            Howdy! I'm Tim Aivazov
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-200 md:text-center lg:text-left">
+            Howdy, dear Guest! <span className='block mt-4'>I'm Tim Aivazov</span>
           </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-100">
             <TypeAnimation
